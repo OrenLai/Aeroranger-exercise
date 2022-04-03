46492114aa81d2ddf08c6535a6ffb62458f9f65b
Oren Lai <oren0602@gmail.com>
2022-04-03 23:25:18 +0800

    add flowchart , set up login page for sending post request

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-var myObject = new Vue({
-    el: '#app',
-    data: {message: 'Hello Vue!'}
-})
+Vue.createApp({
+  data() {
+    return {
+      message: 'Welcome to Aero Ranger ANPR Application',
+      email:'',
+      password:''
+    }
+  },methods:{
+
+    anprLogin(){
+      
+      console.log(this.email, this.password);
+      
+      // use axios to make post request
+      const url = 'https://dashboard.api-aeroranger.com/auth/login/'
+      const params = new URLSearchParams();
+      params.append('email', this.email);
+      params.append('password',this.password);
+      
+      axios.post(url, params)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    }
+
+  }
+}).mount('#app')
