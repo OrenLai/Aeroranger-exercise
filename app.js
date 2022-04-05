0ddb78ac55641c51ab96ccfb7a5ce2e3f70f55bb
Oren Lai <oren0602@gmail.com>
2022-04-05 09:45:52 +0800

    add flag to switch between login/anpr div, display error message when login fail

--- app.js
+++ app.js
@@ -2,29 +2,42 @@ Vue.createApp({
   data() {
     return {
       message: 'Welcome to Aero Ranger ANPR Application',
-      email:'',
-      password:''
+      email: '',
+      password: '',
+      needLogin: true,
+      myToken: '',
+      loginError: ''
     }
   },methods:{
 
-    anprLogin(){
+    async anprLogin(){
       
-      console.log(this.email, this.password);
+      // console.log(this.email, this.password);
       
-      // use axios to make post request
+      //config params
       const url = 'https://dashboard.api-aeroranger.com/auth/login/'
       const params = new URLSearchParams();
       params.append('email', this.email);
       params.append('password',this.password);
-      
-      axios.post(url, params)
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+      //make post request with the params
+      try {
+        const {data} = await axios.post(url, params)
+        console.log(data.id_token);
+        //switch the display from the login div to application div
+        this.needLogin = false
+        //store token
+        this.myToken = 'Bearer '+data.id_token
+        console.log(this.myToken);
+      } catch (error) {
+        // const errorMessage = error.response.data.error.message       
+        this.loginError = error.response.data.error.message + ', please try again'
+        console.log(loginError);
+      }      
+     },
+
+    swapFunction(){
+      this.needLogin = ! this.needLogin
     }
 
-  }
+    },    
 }).mount('#app')
